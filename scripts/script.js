0ee9883750695e81a8e3dc8cf1ffe478fe65b39d
Ítalo Braga <itojk2@hotmail.com>
2022-02-28 16:29:40 -0300

    fixes: request error

---
+++ scripts/script.js
@@ -0,0 +1,47 @@
+const cep = document.getElementById('cep');
+const district = document.getElementById('district');
+const address = document.getElementById('address');
+const city = document.getElementById('city');
+const state = document.getElementById('state');
+
+const erro = document.getElementById('error');
+
+
+const checkCep = () => {
+    if (cep.value === '') {
+       return erro.innerHTML = 'Digite um CEP!';
+    }
+
+    searchCep()
+}
+
+const handleValues = (data) => {
+    erro.innerHTML = ''
+    address.value = data.logradouro;
+    district.value = data.bairro;
+    city.value = data.localidade;
+    state.value = data.uf;
+};
+
+const searchCep = async () => {
+    
+    try {
+        const url = `http://viacep.com.br/ws/${cep.value}/json/`;
+        const response = await fetch(url);
+        const data = await response.json();
+
+        if (data.hasOwnProperty('erro')) {
+            address.value = '';
+            district.value = '';
+            city.value = '';
+            state.value = '';
+            erro.innerHTML = 'CEP não encontrado!';
+            console.log(data)
+            return;
+        }
+
+        handleValues(data);
+    } catch (error) {
+        console.log(error);
+    }
+};
